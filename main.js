6c58efe80f18c30d7b78f3dada85c82e01cc6f69
doouglas <douglasribeiro084@gmail.com>
2024-09-12 12:44:15 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function downloadPdf() {
+    const item = document.querySelector('.content');
+
+    var opt = {
+        margin:       1,
+        filename:     'myfile.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 2 },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    html2pdf().set(opt).from(item).save();
+}
